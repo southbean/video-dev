81a524d649d538bee5df36b17d9819bc497161ea
quanyiting <493852542@qq.com>
2018-11-27 22:03:11 +0800

    6-8 小视频上传联调

--- video-dev-mini-api/src/main/java/com/imooc/controller/BasicController.java
+++ video-dev-mini-api/src/main/java/com/imooc/controller/BasicController.java
@@ -14,7 +14,7 @@ public class BasicController {
 	public static final String USER_REDIS_SESSION = "user-redis-session";
 	
 	// 文件保存的命名空间
-	public static final String FILE_SPACE = "F:\\4_project\\file";
+	public static final String FILE_SPACE = "D:\\4_file\\wxfile\\img\\video";
 	
 	// ffmpeg所在目录
 	public static final String FFMPEG_EXE = "C:\\ffmpeg\\bin\\ffmpeg.exe";
